048cefda0e36d3612014f5dc197c6d38da45bcae
Mathan <nathancompbioakes@gmail.com>
2015-03-24 23:40:57 -0700

    added error handling for incorrect number of inputs

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 #include <string>
+#include <stdexcept>
 #include "ModelHistogram.h"
 
 int main( int argc, char* argv[] ) {
+	if ( argc != 2 ) {
+		throw std::runtime_error( "./ModelHistogram file_name" );
+	}
 	std::string file_in{ argv[1] };
 	theta best = find_theta( file_in );
 	std::cout << "mu=" << best.m_mu << " sigma=" << best.m_sigma << " lambda=" <<
